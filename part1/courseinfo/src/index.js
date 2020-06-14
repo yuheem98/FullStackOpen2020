54e93dcf82b9bedbdaa084b5e8112d6b74f8d386
yuheem98 <yuuuheem@gmail.com>
2020-06-14 18:50:54 +0800

    Add Part component and refactor Content component for part 1 exercise 2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,32 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <p>{props.p1} {props.ex1}</p>
-      <p>{props.p2} {props.ex2}</p>
-      <p>{props.p3} {props.ex3}</p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
-    </div>
-  )
-}
-
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundementals of React'
@@ -37,11 +11,45 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
+    <>
       <Header course={course} />
       <Content p1={part1} p2={part2} p3={part3} 
         ex1={exercises1} ex2={exercises2} ex3={exercises3} />
       <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.p1} exercises={props.ex1} />
+      <Part name={props.p2} exercises={props.ex2} />
+      <Part name={props.p3} exercises={props.ex3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
     </div>
   )
 }
