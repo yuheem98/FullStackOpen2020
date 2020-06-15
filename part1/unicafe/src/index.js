c22604bce000baf52bcd5264088eaeb929e99394
yuheem98 <yuuuheem@gmail.com>
2020-06-15 20:57:19 +0800

    Update unicafe app for part 1 exercise 7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,9 +1,13 @@
 import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
+const Header = ({ text }) => (
+  <h1>{text}</h1>
+)
+
+const FeedbackButton = ({ handleClick, type }) => (
+  <button onClick={handleClick(type)}>
+    {type}
   </button>
 )
 
@@ -13,28 +17,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGoodClick = () => {
-    setGood(good + 1)
+  const handleButtonType = (type) => () => {
+    if (type === 'good') { setGood(good + 1) }
+    if (type === 'neutral') { setNeutral(neutral + 1) }
+    if (type === 'bad') { setBad(bad + 1) }
   }
+  
+  const total = good + neutral + bad
 
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-  }
+  const average = total === 0 ? 0 : (good - bad) / total
 
-  const handleBadClick = () => {
-    setBad(bad + 1)
-  }
+  const positive = total === 0 ? 0 : good / total * 100
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handleClick={handleGoodClick} text='good' />
-      <Button handleClick={handleNeutralClick} text='neutral' />
-      <Button handleClick={handleBadClick} text='bad' />
-      <h1>statistics</h1>
+      <Header text='Give Feedback' />
+      <FeedbackButton handleClick={handleButtonType} type='good' />
+      <FeedbackButton handleClick={handleButtonType} type='neutral' />
+      <FeedbackButton handleClick={handleButtonType} type='bad' />
+      <Header text='Statistics' />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
