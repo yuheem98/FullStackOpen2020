b13e39c85d6061b8120e8784c956b9ac38a03288
yuheem98 <yuuuheem@gmail.com>
2020-06-15 21:33:23 +0800

    Update unicafe app for part 1 exercise 9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -17,8 +17,16 @@ const Statistic = ({ text, value}) => (
 
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
-  const average = total === 0 ? 0 : (good - bad) / total
-  const positive = (total === 0 ? 0 : good / total * 100) + ' %'
+  const average = (good - bad) / total
+  const positive = good / total * 100 + ' %'
+  
+  if (total === 0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
 
   return (
     <div>
