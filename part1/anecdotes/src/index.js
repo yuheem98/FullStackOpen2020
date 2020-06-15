7ee26e3c3bc4e22eb540cfa695ff5e93fe74c3a7
yuheem98 <yuuuheem@gmail.com>
2020-06-15 23:10:02 +0800

    Update anecdotes app for part 1 exericse 13

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -9,22 +9,31 @@ const Button = ({ handleClick, text }) => (
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-  
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
   const getRandomInt = (min, max) => {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
-  const setSelectedValue = (min, max) => () => {
-    const randomInt = getRandomInt(min, max)
+  const handleNextClick = () => {
+    const randomInt = getRandomInt(0, anecdotes.length - 1)
     setSelected(randomInt)
+  }
+  
+  const handleVoteClick = () => {
+    const copyVotes = [...votes]
+    copyVotes[selected] += 1
+    setVotes(copyVotes)
   }
 
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <Button handleClick={setSelectedValue(0, 5)} 
+      <p>{props.anecdotes[selected]}<br /> has {votes[selected]} votes</p>
+      <Button handleClick={handleVoteClick}
+        text='Vote' />
+      <Button handleClick={handleNextClick} 
         text='Next anecdote' />
     </div>
   )
@@ -34,14 +43,14 @@ const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of ' +
-  'the development time...The remaining 10 percent of the code accounts ' +
-  'for the other 90 percent of the development time.',
+    'the development time...The remaining 10 percent of the code accounts ' +
+    'for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers ' +
-  'write code that humans can understand.',
+    'write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. ' +
-  'Therefore, if you write the code as cleverly as possible, you are, by ' +
-  'definition, not smart enough to debug it.'
+    'Therefore, if you write the code as cleverly as possible, you are, by ' +
+    'definition, not smart enough to debug it.'
 ]
 
 ReactDOM.render(
