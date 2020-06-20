313ed59a6e7cb38fde991b0a6d9cbe0ed06ac702
yuheem98 <yuuuheem@gmail.com>
2020-06-20 14:21:20 +0800

    Update phonebook app for part 2 exerices 9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -19,10 +19,14 @@ const Persons = ({ persons }) => {
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    {name: 'Arto Hellas', number: '040-1234567'}
+    {name: 'Arto Hellas', number: '040-1234567'},
+    {name: 'Ada Lovelace', number: '39-44-5323523' },
+    {name: 'Dan Abramov', number: '12-43-234345'},
+    {name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
   
   const addPerson = (event) => {
     event.preventDefault()
@@ -43,23 +47,34 @@ const App = () => {
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handeNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setNewFilter(event.target.value)
+  
+  const personsToShow = newFilter 
+    ? persons.filter(person => 
+      person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    : persons
 
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          Filter shown with 
+          <input value={newFilter} onChange={handleFilterChange} />
+        </div>
+      <h2>Add a new entry</h2>
       <form onSubmit={addPerson}>
         <div>
-          name:<input value={newName} onChange={handleNameChange} />
+          Name:<input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number:<input value={newNumber} onChange={handeNumberChange} />
+          Number:<input value={newNumber} onChange={handeNumberChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        <Persons persons={persons} />
+        <Persons persons={personsToShow} />
     </div>
   )
 }
